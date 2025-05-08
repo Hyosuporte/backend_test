2868f1289df1f2bc2865b4c9e27bd2dcca97dab4
hyosuporte <cacuervo120@gmail.com>
2025-05-07 22:30:31 -0500

    modificaciones respuesta

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # backend_test
-Backend test with python 
-* Analyze the code present in the test
-* What does this code do?
-* Which segments would you change, why?
-* Could you make a better version?
-* Now do it and enjoy it
+
+Backend test with python
+
+- ## Analyze the code present in the test
+
+- ## What does this code do?
+
+      El código recibe un evento donde este está compuesto por el Id del usuario y el Id del dispositivo.
+
+      Con esta información, primero se obtiene el usuario y se valida si tiene uno de los roles suficientes para poder continuar con la operación. En caso de que la validación sea correcta, se obtiene el dispositivo por su Id, y luego con esa información se obtiene una lista de productos, donde al final estos se retornan junto a su valor.
+
+- ## Which segments would you change, why?
+
+        Cambiaría la parte de la asignación de los valores de products, products_width y flag, moviendo su inicialización a otro lugar para poder controlarlos de manera más sencilla y mantener el lambda_handler más limpio y centrado en su objetivo principal.
+
+        Por último, también ajustaría la forma en la que se asignan los valores de los widths. En lugar de usar el índice i, utilizaría la función append() para seguir la misma lógica que se usa al construir el array de products, logrando así que todo el código sea más consistente entre sí.
+
+- ## Could you make a better version?
+
+        Sí, considero que aplicando los cambios que mencioné — como mover la inicialización de products, products_width y flag fuera del handler principal — se puede tener un mejor control del código y mantener el lambda_handler más enfocado en su lógica principal.
+
+        Además, separar las funciones de obtención de datos en un archivo auxiliar también podría ser una buena mejora para mantener esta parte más limpia y organizada.
+
+- ## Now do it and enjoy it
--- interview.py
+++ interview.py
@@ -14,138 +14,139 @@ import boto3
 #########################
 """
 
+
 def get_user_on_dynamoDB(id):
-    invokeLambda = boto3.client('lambda', region_name='us-east-2')
-    lambda_response = invokeLambda.invoke(FunctionName = 'DA_DynamoDB_Read', InvocationType = 'RequestResponse', 
-        Payload = json.dumps({
-            "table":"user",
-            "key_name":"id",
-            "key_value":id
-        }))
-    resp_str = lambda_response['Payload'].read()
+    invokeLambda = boto3.client("lambda", region_name="us-east-2")
+    lambda_response = invokeLambda.invoke(
+        FunctionName="DA_DynamoDB_Read",
+        InvocationType="RequestResponse",
+        Payload=json.dumps({"table": "user", "key_name": "id", "key_value": id}),
+    )
+    resp_str = lambda_response["Payload"].read()
     resp = json.loads(resp_str)
     return resp[0], resp[1]
 
+
 def get_iot_device_on_dynamoDB(id):
-    invokeLambda = boto3.client('lambda', region_name='us-east-2')
-    lambda_response = invokeLambda.invoke(FunctionName = 'DA_DynamoDB_Read', InvocationType = 'RequestResponse', 
-        Payload = json.dumps({
-            "table":"iotDevice",
-            "key_name":"id",
-            "key_value":id
-        }))
-    resp_str = lambda_response['Payload'].read()
+    invokeLambda = boto3.client("lambda", region_name="us-east-2")
+    lambda_response = invokeLambda.invoke(
+        FunctionName="DA_DynamoDB_Read",
+        InvocationType="RequestResponse",
+        Payload=json.dumps({"table": "iotDevice", "key_name": "id", "key_value": id}),
+    )
+    resp_str = lambda_response["Payload"].read()
     resp = json.loads(resp_str)
     return resp[0], resp[1]
-    
+
+
 def get_iot_device_on_dynamoDB(id):
-    invokeLambda = boto3.client('lambda', region_name='us-east-2')
-    lambda_response = invokeLambda.invoke(FunctionName = 'DA_DynamoDB_Read', InvocationType = 'RequestResponse', 
-        Payload = json.dumps({
-            "table":"iotDevice",
-            "key_name":"id",
-            "key_value":id
-        }))
-    resp_str = lambda_response['Payload'].read()
+    invokeLambda = boto3.client("lambda", region_name="us-east-2")
+    lambda_response = invokeLambda.invoke(
+        FunctionName="DA_DynamoDB_Read",
+        InvocationType="RequestResponse",
+        Payload=json.dumps({"table": "iotDevice", "key_name": "id", "key_value": id}),
+    )
+    resp_str = lambda_response["Payload"].read()
     resp = json.loads(resp_str)
     return resp[0], resp[1]
+
 
 def get_iot_device_products_on_dynamoDB(deviceId):
-    invokeLambda = boto3.client('lambda', region_name='us-east-2')
-    lambda_response = invokeLambda.invoke(FunctionName = 'DA_DynamoDB_Read', InvocationType = 'RequestResponse', 
-        Payload = json.dumps({
-            "table":"iotDeviceProducts",
-            "key_name":"deviceId",
-            "key_value":deviceId
-        }))
-    resp_str = lambda_response['Payload'].read()
+    invokeLambda = boto3.client("lambda", region_name="us-east-2")
+    lambda_response = invokeLambda.invoke(
+        FunctionName="DA_DynamoDB_Read",
+        InvocationType="RequestResponse",
+        Payload=json.dumps(
+            {
+                "table": "iotDeviceProducts",
+                "key_name": "deviceId",
+                "key_value": deviceId,
+            }
+        ),
+    )
+    resp_str = lambda_response["Payload"].read()
     resp = json.loads(resp_str)
     return resp[0], resp[1]
+
 
 def get_product_on_dynamoDB(id):
-    invokeLambda = boto3.client('lambda', region_name='us-east-2')
-    lambda_response = invokeLambda.invoke(FunctionName = 'DA_DynamoDB_Read', InvocationType = 'RequestResponse', 
-        Payload = json.dumps({
-            "table":"product",
-            "key_name":"id",
-            "key_value":id
-        }))
-    resp_str = lambda_response['Payload'].read()
+    invokeLambda = boto3.client("lambda", region_name="us-east-2")
+    lambda_response = invokeLambda.invoke(
+        FunctionName="DA_DynamoDB_Read",
+        InvocationType="RequestResponse",
+        Payload=json.dumps({"table": "product", "key_name": "id", "key_value": id}),
+    )
+    resp_str = lambda_response["Payload"].read()
     resp = json.loads(resp_str)
     return resp[0], resp[1]
 
-def lambda_handler(event, context):
+
+def lambda_handler(event):
     flag, user_entity = get_user_on_dynamoDB(event["userId"])
     if flag and user_entity:
         flag = False
-        for role in user_entity["roles"]:
-            if role == 1:
-                flag = True
-            if role == 3:
-                flag = True
+        flag = get_role(user_entity)
         if flag:
             flag_device, device = get_iot_device_on_dynamoDB(event["deviceId"])
             if flag_device and device:
-                flag_device_products, device_products = get_iot_device_products_on_dynamoDB(event["deviceId"])
+                flag_device_products, device_products = (
+                    get_iot_device_products_on_dynamoDB(event["deviceId"])
+                )
                 if flag_device_products and device_products:
-                    products = []
-                    for i in range(len(device_products)):
-                        flagProduct, product = get_product_on_dynamoDB(device_products[i]["productId"])
-                        if flagProduct and product:
-                            products.append(product)
-                        else:
-                            return {
-                                'statusCode': 500,
-                                'body': {
-                                    'msg': "Unable to get whole products"
-                                }
-                            }
-                    for i in range(len(products)):
-                        product = products[i]
-                        for j in range(len(device_products)):
-                            if device_products[j]["productId"] == product["id"]:
-                                product["amount"] = device_products[j]["amount"]
-                        products[i] = product
-                    products_width=[]
-                    for i in range(len(products)):
-                        products_width[i] = products[i]["width"]
+                    products = load_products(device_products)
+                    if not products:
+                        return {
+                            "statusCode": 500,
+                            "body": {"msg": "Unable to get whole products"},
+                        }
+
+                    products_width = load_width(products)
+
                     return {
-                        'statusCode': 200,
-                        'body': {"products":list_products, "widths":products_width}
+                        "statusCode": 200,
+                        "body": {"products": products, "widths": products_width},
                     }
+
                 else:
                     return {
-                        'statusCode': 500,
-                        'body': {
-                            'msg': "Unable to get device products"
-                        }
+                        "statusCode": 500,
+                        "body": {"msg": "Unable to get device products"},
                     }
             else:
-                return {
-                    'statusCode': 500,
-                    'body': {
-                        'msg': "Unable to get device"
-                    }
-                }
+                return {"statusCode": 500, "body": {"msg": "Unable to get device"}}
         if not flag:
-            return {
-                'statusCode': 500,
-                'body': {
-                    'msg': "Unauthorized access"
-                }
-            }
+            return {"statusCode": 500, "body": {"msg": "Unauthorized access"}}
     else:
-        return {
-            'statusCode': 500,
-            'body': {
-                'msg': "Unable to retrieve user"
-            }
-        }
-    return {
-        'statusCode': 500,
-        'body': {
-            'msg': "Unable to retrieve user"
-        }
-    }
-        
-            
+        return {"statusCode": 500, "body": {"msg": "Unable to retrieve user"}}
+    return {"statusCode": 500, "body": {"msg": "Unable to retrieve user"}}
+
+
+def get_role(user_entity):
+    for role in user_entity["roles"]:
+        if role == 1 or role == 3:
+            return True
+
+
+def load_products(device_products):
+    products = []
+    for i in range(len(device_products)):
+        flagProduct, product = get_product_on_dynamoDB(device_products[i]["productId"])
+        if flagProduct and product:
+            products.append(product)
+        else:
+            return False
+
+    for i in range(len(products)):
+        product = products[i]
+        for j in range(len(device_products)):
+            if device_products[j]["productId"] == product["id"]:
+                product["amount"] = device_products[j]["amount"]
+        products[i] = product
+
+    return products
+
+
+def load_width(products):
+    products_width = []
+    for i in range(len(products)):
+        products_width.append(products[i]["width"])
